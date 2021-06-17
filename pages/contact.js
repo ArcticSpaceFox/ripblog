2452fe306b3e4f6df6613dfa9597f29dd889a07f
ArcticSpaceFox <Sm03leBr00t@protonmail.com>
2021-06-17 12:52:59 +0200

    :lipstick: added dark ui thme for success popup contactpage

--- pages/contact.js
+++ pages/contact.js
@@ -35,9 +35,9 @@ export default function Projects() {
         </div>
 
         <div className="container items-center px-5 py-12 lg:px-20">
-          {isSubmitSuccessful && (
+          {!isSubmitSuccessful && (
             <div className="container items-center">
-              <div className="w-full text-green-600 border rounded-lg shadow-xl ">
+              <div className="w-full text-green-600 dark:text-green-500 overflow-hidden dark:bg-gray-800 border dark:border-gray-700 rounded-lg shadow-xl ">
                 <div className="flex items-center justify-between px-6 py-4 mx-auto ">
                   <div className="flex">
                     <svg
